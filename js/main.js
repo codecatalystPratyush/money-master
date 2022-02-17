2cc7a15fbe5215d03edc4ba6f45aee8c514320d9
genuinestalwart <genuinestalwart@gmail.com>
2022-02-17 17:49:26 +0600

    functionality added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
 window.addEventListener('load', () => {
     document.getElementById('calculate-btn').addEventListener('click', () => {
+        const values = [invalid('income'), invalid('food'), invalid('rent'), invalid('clothes')];
 
+        if (values[0] == 'invalid' || values[1] == 'invalid' || values[2] == 'invalid' || values[3] == 'invalid') {
+            // error modal
+        } else {
+            const expenses = values[1] + values[2] + values[3];
+
+            if (expenses > values[0]) {
+                // error modal
+            } else {
+                document.getElementById('total-expenses').innerText = expenses;
+                document.getElementById('balance').innerText = values[0] - expenses;
+            }
+        }
     });
+
     document.getElementById('save-btn').addEventListener('click', () => {
+        const balance = parseInt(document.getElementById('balance').innerText);
+        const values = [invalid('income'), invalid('save')];
+
+        if (values[0] == 'invalid' || values[1] == 'invalid' || values[1] > 100) {
+            // error modal
+        } else {
+            const amount = values[0] * values[1] / 100;
+
+            if (amount > balance) {
+                // error modal
+            } else {
+                document.getElementById('saving-amount').innerText = amount;
+                document.getElementById('remaining-balance').innerText = balance - amount;
+            }
+        }
 
     });
 });
+
+function invalid(idName) {
+    const input = parseInt(document.getElementById(idName).value);
+
+    if (isNaN(input) || input < 0) {
+        return 'invalid';
+    } else {
+        return input;
+    }
+}
