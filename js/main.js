ec3508c0849e77b03ca56f4bf62d6bc81310541e
genuinestalwart <genuinestalwart@gmail.com>
2022-02-17 14:52:40 +0600

    id for event listeners added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    document.getElementById('calculate-btn').addEventListener('click', () => {
+
+    });
+    document.getElementById('save-btn').addEventListener('click', () => {
+
+    });
+});
